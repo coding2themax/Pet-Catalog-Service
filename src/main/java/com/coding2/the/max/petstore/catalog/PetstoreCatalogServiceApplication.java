f853bd099df1f909e4b9c5a81f9b5e7ed8138b10
max quinones <maximilian.quinones@gmail.com>
2025-08-03 10:27:20 -0700

    init

---
+++ src/main/java/com/coding2/the/max/petstore/catalog/PetstoreCatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2.the.max.petstore.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstoreCatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstoreCatalogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding2/the/max/petstore/catalog/PetstoreCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2.the.max.petstore.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstoreCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
